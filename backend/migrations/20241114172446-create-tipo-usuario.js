7b813062b186df5ff0ac5af76169749b8f150e85
Eidrian2005 <eserrano@fwdcostarica.com>
2024-11-18 11:28:30 -0600

    ajuste en el nombre de una tabla

--- backend/migrations/20241114172446-create-tipo-usuario.js
+++ backend/migrations/20241114172446-create-tipo-usuario.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('tipo_usuarios', {
+    await queryInterface.createTable('tipo_usuario', {
       id: {
         allowNull: false,
         autoIncrement: true,
