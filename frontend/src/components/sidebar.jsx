4f40a734165d73937c71ebb81732f3022f033a32
Nixon Davila <davilanixon8@gmail.com>
2024-12-13 13:26:57 -0600

    estilos

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,6 +1,9 @@
 // Sidebar Component in React
 import React, { useState } from 'react';
 import '../styles/sidebar.css'; // Import the CSS file
+// import '../styles/header.css'
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Sidebar = () => {
     const [isHovered, setIsHovered] = useState(false);
@@ -11,24 +14,41 @@ const Sidebar = () => {
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
         >
+            <div className='contenedor'>
             <a href="#">
-                <span className="icon">🏠</span>
-                <span className="text">Inicio</span>
+            <p className="nav-item">
+                <Link to="/ListaDeseados" className="nav-link">
+                Wishlist
+                </Link>
+                </p>
             </a>
             <a href="#">
-                <span className="icon">📄</span>
-                <span className="text">Documentos</span>
+            <p className="nav-item">
+                <Link to="/Login" className="nav-link">
+                Login
+                </Link>
+                </p>
             </a>
             <a href="#">
-                <span className="icon">⚙️</span>
-                <span className="text">Configuración</span>
+            <p className="nav-item cart">
+                <Link to="/carrito" className="nav-link">
+                <i className="fas fa-shopping-cart"></i>
+                </Link>
+                </p>
             </a>
             <a href="#">
                 <span className="icon">❓</span>
                 <span className="text">Ayuda</span>
             </a>
+            </div>
         </div>
     );
 };
 
 export default Sidebar;
+
+
+
+
+
+
