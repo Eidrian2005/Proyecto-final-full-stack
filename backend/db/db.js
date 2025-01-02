ba7b95856958119e0a896c231015339ca5ea235a
Nixon Davila <davilanixon8@gmail.com>
2025-01-02 11:25:06 -0600

    areglo de carsd

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize('shedaboshop', 'eidrian', 'EDGAV20052506', {
-host: '192.168.1.80',
+host: '192.168.100.7',
 dialect: 'mysql',
 });
 module.exports = sequelize;
